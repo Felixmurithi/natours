9882b2e28463a43dcbdc00859b4972dcb063d847
Felixmurithi <murithifelixgims@gmail.com>
2024-02-06 12:29:49 +0300

    immplemenetd cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hpp = require('hpp');
 const helmet = require('helmet');
 const cookieParse = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,11 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+// app.options('/api/v1/tours/:id', cors());
+
+app.options();
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
